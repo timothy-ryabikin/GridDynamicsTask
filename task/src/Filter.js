82a798fa12cf37e9928615e967bdd460f4653f15
Timothy <02.timoha.0@gmail.com>
2021-08-10 17:36:19 +0300

    Refactoring

--- task/src/Filter.js
+++ task/src/Filter.js
@@ -3,31 +3,13 @@ import './css/Filter.css';
 
 function Filter(props) {
 
-    const { tickers, setTickers, filteredTickers, setFilteredTickers } = props;
+    const { filteredTickers, setFilteredTickers } = props;
 
     const [search, setSearch] = useState("");
-
-    const [dummy, setDummy] = useState();
 
     const handleChangeName = (e) => {
         setSearch(e.target.value);
     }
-
-    // const filterContent = (e) => {
-    //     e.preventDefault();
-    //     const regexp = new RegExp(search, 'i');
-    //     const filter = tickers.filter(ticker => regexp.test(ticker.name));
-    //     setFilteredTickers(filter);
-    //     if (filteredTickers.length != 0) {
-    //         setTickerOrFilter(true);
-    //         console.log(filteredTickers);
-    //     }
-    //     else (setTickerOrFilter(false))
-    //     if (search == "") {
-    //         setFilteredTickers([]);
-    //         console.log(tickerOrFilter);
-    //     }
-    // }
 
     const filterContent = (e) => {
         e.preventDefault();
